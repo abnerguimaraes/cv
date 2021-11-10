29c6dd09a2d2f0650c07a869ead0109b37341b7d
ABNER THIAGO DA SILVA GUIMARAES <abner.guimaraes@bradesco.com.br>
2021-11-10 09:47:02 -0300

    Update Cloud

---
+++ public/js/index.js
@@ -0,0 +1,10 @@
+var linkedIn = document.getElementById("lknLinked");
+var mail     = document.getElementById("lknMail");
+
+linkedIn.addEventListener("click", function(){
+    window.open("https://www.linkedin.com/in/abnerguimaraes/", "_blank")
+});
+
+mail.addEventListener("click", function(){
+    window.open("mailto:guimaraesabner@gmail.com", "_blank");
+});
